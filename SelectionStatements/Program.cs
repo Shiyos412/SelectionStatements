8eb7455304995906d1e45d629678a5b0577baee1
Michael Cerase <michaelcerase@gmail.com>
2020-01-21 12:53:57 -0600

    First commit, guessing if statements

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var favorite = 4;
+            var guess = int.Parse(Console.ReadLine());
+            if(guess < favorite)
+            {
+                Console.WriteLine("Too low");
+            }
+            else if( guess > favorite)
+            {
+                Console.WriteLine("Too high");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
